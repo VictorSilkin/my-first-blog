12233711317dfe919254b4dc296630a45a22fb5a
VictorSilkin <victor_silkin@mail.ru>
2018-02-19 13:36:09 +0300

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,7 @@
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.auth import views
+from mysite.views import hello
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-	url(r'^accounts/login/$', views.login, name='login'),
-	url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
-	url(r'', include('blog.urls')),
+	url('^$', hello),
 ]
